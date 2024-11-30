e34bc68aed63dd66f51a6b9c6dd450d5527c10a9
Akif Akbulut <akifakb@gmail.com>
2024-11-30 11:52:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    internal class Program
+    {
+        public class Node
+        {
+            public int x;
+            public Node left;
+            public Node right;
+        }
+        public class BinarySearchTree
+        {
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
